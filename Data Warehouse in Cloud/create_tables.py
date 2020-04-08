a9d6a3848d22aad64cec4aea35f3f120fd1a7a6d
Sagar Dhungel <hiesagar@gmail.com>
2020-04-08 11:40:15 +0545

    docstrings added

--- Data Warehouse in Cloud/create_tables.py
+++ Data Warehouse in Cloud/create_tables.py
@@ -4,18 +4,37 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    responsible for deleting pre-existing tables to ensure that our database does not throw any error
+    Parameters:
+        curr: cursor for the database
+        conn: database connection made using conection parameters 
+    Returns:
+        None
+   """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    responsible for creating pre-existing tables
+    Parameters:
+        curr: cursor for the database
+        conn: database connection made using conection parameters 
+    Returns:
+        None
+   """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    this function uses configuration parameters to create database connection and cursor to drop and create tables
+   """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- Data Warehouse in Cloud/etl.py
+++ Data Warehouse in Cloud/etl.py
@@ -4,21 +4,39 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    responsible for staging pre-existing tables to ensure that our database does not throw any error
+    Parameters:
+        curr: cursor for the database
+        conn: database connection made using conection parameters 
+    Returns:
+        None
+   """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    responsible for inserting data to  tables
+    Parameters:
+        curr: cursor for the database
+        conn: database connection made using conection parameters 
+    Returns:
+        None
+   """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    this function uses configuration parameters to create database connection and loads and insert data into staging table
+   """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
