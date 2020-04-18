e98b2f950227ad99de40cb163bdb5ce7a5b63d12
Sagar Dhungel <hiesagar@gmail.com>
2020-04-18 17:51:48 +0545

    fix ambiguity on column name year

--- Data Lakes/etl.py
+++ Data Lakes/etl.py
@@ -39,16 +39,23 @@ def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
     song_data = os.path.join(input_data,"song_data/A/B/C/TRABCEI128F424C983.json")
     #song_data = os.path.join(input_data,"song_data/*/*/*/*.json")
+    
     # read song data file
     df = spark.read.json(song_data)
     # extract columns to create songs table
     songs_table = df['song_id', 'title', 'artist_id', 'year', 'duration']
+    
+    print("Songs table schema")
+    songs_table.printSchema()
     
     # write songs table to parquet files partitioned by year and artist
     songs_table.write.partitionBy('year', 'artist_id').parquet(os.path.join(output_data, 'songs.parquet'), 'overwrite')
 
     # extract columns to create artists table
     artists_table = df['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+    
+    print("Artists table schema")
+    artists_table.printSchema()
     
     # write artists table to parquet files
     artists_table.write.parquet(os.path.join(output_data, 'artists.parquet'), 'overwrite')
@@ -63,24 +70,31 @@ def process_log_data(spark, input_data, output_data):
     outputdata = output data directory (s3)
     spark = sprak session object
     """
+    
     # get filepath to log data file
-    log_data = input_data + 'log_data/*/*/'
+    log_data = input_data + 'log_data/2018/11/2018-11-12-events.json'
+    #log_data = input_data + 'log_data/*/*/*.json'
 
+ 
     # read log data file
-    print('Reading in log data')
     df = spark.read.json(log_data)
     
     # filter by actions for song plays
     df = df[df['page'] == 'NextSong']
-
+    
+    print("schama from log data")
+    df.printSchema()
+    
     # extract columns for users table   
-    artists_table = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    users_table = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    users_table = users_table.drop_duplicates(subset=['userId'])
+    print("User table from log data")
+    users_table.printSchema()
     
     # write users table to parquet files
-    artists_table.write.parquet(output_data+'/users_table')
+    users_table.write.parquet(output_data+'/users_table')
 
     # create timestamp column from original timestamp column
-    print('Creating time table')
     get_timestamp = udf(lambda x: datetime.fromtimestamp(x / 1000.0), TimestampType())
     df = df.withColumn('start_time', get_timestamp(df.ts))
     
@@ -97,13 +111,23 @@ def process_log_data(spark, input_data, output_data):
     # extract columns to create time table
     time_table = df[['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']]
     
+    print("Time table from log data")
+    time_table.printSchema()
+    
     # write time table to parquet files partitioned by year and month
     time_table.write.partitionBy('year','month').parquet(output_data+'/timetable')
 
-    # read in song data to use for songplays table
-    print('Creating songplays table')
-    song_df = spark.read.json(input_data + 'song_data/*/*/*/*.json')
-
+    print("Reading Song data")
+    # read in song data to use for songplays table 
+    song_df = spark.read.json(input_data + "song_data/A/B/C/TRABCEI128F424C983.json")
+    #song_df = spark.read.json(input_data + "song_data/*/*/*/*.json")
+    
+    print("Written Song Data")
+    song_df.printSchema()    
+    
+    # drop 'year' from dataframe to resolve ambiguity in column name 
+    df = df.drop("year")
+    
     # extract columns from joined song and log datasets to create songplays table 
     songplays_table = df.join(song_df, song_df.artist_name == df.artist)
     songplays_table = songplays_table.withColumn("songplay_id",F.monotonically_increasing_id())
@@ -117,8 +141,8 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = "s3a://buckudacity/data"
+    input_data = ""
+    output_data = "s3a://my-awesome-buck/data"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
